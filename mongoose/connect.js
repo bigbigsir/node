00f1624032862a661ef5ff30f8f93835f524de82
emoji <519628061@qq.com>
2020-06-01 11:30:16 +0800

    first commit

---
+++ mongoose/connect.js
@@ -0,0 +1,20 @@
+const config = require('../config/config')
+const mongoose = require('mongoose')
+const db = mongoose.connect(config.dbPath, config.dbOptions)
+
+// 连接成功
+mongoose.connection.on('connected', () => {
+  console.log(`Mongoose connection open to ${config.dbPath}`.green.bold)
+})
+
+// 连接异常
+mongoose.connection.on('error', err => {
+  console.log(`Mongoose connection error: ${err}`.red.bold)
+})
+
+// 连接断开
+mongoose.connection.on('disconnected', () => {
+  console.log('Mongoose connection disconnected'.red.bold)
+})
+
+module.exports = db
